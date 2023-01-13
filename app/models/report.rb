b82db4ff30835ece964b526f0c78c6e070de2bf0
yocajii <yocajii@stone.sakura.ne.jp>
2023-01-13 09:10:40 +0900

    Mentionモデルを編集した

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,22 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :active_mentions,
+           class_name: 'Mention',
+           foreign_key: 'mentioned_report_id',
+           dependent: :destroy,
+           inverse_of: :mentioned_report
+  has_many :passive_mentions,
+           class_name: 'Mention',
+           foreign_key: 'mentioning_report_id',
+           dependent: :destroy,
+           inverse_of: :mentioning_report
+
+  has_many :mentioning_report,
+           through: :active_mentions
+  has_many :mentioned_report,
+           through: :passive_mentions
+
   validates :title, presence: true
   validates :content, presence: true
 
