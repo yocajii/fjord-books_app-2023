1731ae97ad54cdf0e6f3c2197311c35805c2aec5
yocajii <yocajii@stone.sakura.ne.jp>
2023-01-18 11:01:15 +0900

    ユニーク制約のバリデーションを追加

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,6 @@
 class Mention < ApplicationRecord
   belongs_to :mentioning_report, class_name: 'Report'
   belongs_to :mentioned_report, class_name: 'Report'
+
+  validates :mentioning_report, uniqueness: { scope: %i[mentioning_report mentioned_report] }, presence: true
 end
